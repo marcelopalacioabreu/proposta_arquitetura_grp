0b31da9fff65dcdd3dcc1c8bb3e7df585a1dac52
Marcelo Palacio Abreu <marcelopalacioabreu@gmail.com>
2026-08-05 14:55:24 -0300

    Alteração de MultiLocatarioEntidade

--- src/retaguarda/Dominio/Entidades/Base/MultilocatarioEntidade.cs
+++ src/retaguarda/Dominio/Entidades/Base/MultilocatarioEntidade.cs
@@ -5,11 +5,16 @@ namespace Retaguarda.Dominio.Entidades.Base
     public abstract class MultilocatarioEntidade
     {
         public long Id { get; set; }
+        public Guid IdentificadorUnico { get; set; } = Guid.NewGuid();
+        public string IdentificadorUnicoAmigavel { get; set; } = "";
         public DateTime DataInsercao { get; set; } = DateTime.UtcNow;
-        public DateTime? DataAlteracao { get; set; }
+        public DateTime? DataAlteracao { get; set; } = DateTime.UtcNow;
         public long? OrganizacaoId { get; set; }
         public long? OrganizacaoUnidadeId { get; set; }
         public long? SetorId { get; set; }
         public bool Ativo { get; set; } = true;
+        public long? UsuarioInsercaoId { get; set; }
+        public long? UsuarioAlteracaoId { get; set; }
+        public long Versao { get; set; } = 1;
     }
 }
