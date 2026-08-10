a41cdc58000591807bf8173c052dbdada7976ba3
Marcelo Palacio Abreu <marcelopalacioabreu@gmail.com>
2026-08-10 15:18:57 -0300

    Ajuste no layout da página de incorporação do Elsa e posicionamento do botão da gaveta do menu.

--- src/interface_grafica/web/src/paginas/painel/PainelPlanejadorFluxo.jsx
+++ src/interface_grafica/web/src/paginas/painel/PainelPlanejadorFluxo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function PainelPlanejadorFluxo(){
   const src = '/planejadorDeFluxo/studio' // proxy path; target configured in PlanejadorFluxo service
   return (
-    <div style={{height: '100%', width: '100%'}}>
+    <div style={{height: '100%', width: '100%', margin: '-16px -12px -16px 20px'}}>
       <iframe title="Planejador de Fluxo" src={src} style={{border:0, width:'100%', height:'90vh'}} />
     </div>
   )
