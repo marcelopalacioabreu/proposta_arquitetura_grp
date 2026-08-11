30d280db9add85a7fa03c9244fd8babb744d3901
Marcelo Palacio Abreu <marcelopalacioabreu@gmail.com>
2026-08-11 16:59:29 -0300

    Projeto DTO

---
+++ src/retaguarda/Retaguarda.DTO/Dtos/OrquestracaoFluxoProcessoDto.cs
@@ -0,0 +1,12 @@
+namespace Retaguarda.DTO.Dtos
+{
+    public class OrquestracaoFluxoProcessoDto
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+        public string? WorkflowDefinitionId { get; set; }
+        public int? WorkflowVersion { get; set; }
+        public bool Ativo { get; set; } = true;
+    }
+}
