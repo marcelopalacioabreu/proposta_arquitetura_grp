55228b5e54d7bed35e8bc6187c57905e0b4a5f9f
Marcelo Palacio Abreu <marcelopalacioabreu@gmail.com>
2026-08-26 13:35:47 -0300

    Ajuste do endereço

--- src/retaguarda/Dominio/Entidades/Endereco.cs
+++ src/retaguarda/Dominio/Entidades/Endereco.cs
@@ -4,16 +4,18 @@ namespace Retaguarda.Dominio.Entidades
 {
     public class Endereco : MultilocatarioEntidade
     {
-        public long UsuarioId { get; set; }
         public long? PaisId { get; set; }
         public long? UfId { get; set; }
         public long? MunicipioId { get; set; }
         public long? BairroId { get; set; }
         public long? LogradouroId { get; set; }
         public long CepId { get; set; }
-        public string Complemento { get; set; } = string.Empty;
-
-        public Usuario? Usuario { get; set; }
+        public EnderecoPais? Pais { get; set; }
+        public EnderecoUF? Uf { get; set; }
+        public EnderecoMunicipio? Municipio { get; set; }
+        public EnderecoBairro? Bairro { get; set; }
+        public EnderecoLogradouro? Logradouro { get; set; }
         public EnderecoCEP? Cep { get; set; }
+        public string Complemento { get; set; } = string.Empty;
     }
 }
